9393d0d1e230990941b0ce6a13483fe27a862f5b
Mithali Redkar <86156837+Mitz190@users.noreply.github.com>
2021-08-23 15:38:04 +0530

    Kids with greatest number of candies file created

---
+++ Kidswithgreatestnumberofcandies.java
@@ -0,0 +1,22 @@
+//using arrayList in java
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> ans =new ArrayList<>(); 
+        for(int i=0;i<candies.length;i++){
+            boolean test =false;
+            for(int j=0;j<candies.length;j++){
+                if(candies[i]+extraCandies<candies[j]){
+                    test=true;
+                    break;
+                }
+            }
+            if (test){
+                ans.add(false);
+            }
+            else{
+                ans.add(true);
+            }
+        }
+        return ans;
+    }
+}
